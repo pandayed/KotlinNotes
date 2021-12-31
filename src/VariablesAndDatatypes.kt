22b9af0acb3bdff265893f51c72611b47e1ff6af
xpandeyed <lalbiharipandeyg@gmail.com>
2021-12-31 17:06:23 +0530

    Var, val and datatypes added.

---
+++ src/VariablesAndDatatypes.kt
@@ -0,0 +1,27 @@
+fun varAndVal(){
+
+    //ctrl+shift+p on identifier shows its datatype
+
+    //var can be reassigned
+    var num = 10
+    println(num)
+    num = 20
+    println(num)
+
+    //val can not be reassigned
+    val myString = "Hello"
+    println(myString)
+
+    //val or var both need not be initialized, but for that we need to specify datatype
+}
+
+fun dataTypes(){
+
+    var score : Int = 10
+    var temperature: Float = 12F
+    var moles: Double = 2.2233445566
+    var name : String = "Lal Bihari Pandey"
+    var isItRaining : Boolean = false
+
+    //Float datatype values must end with F or f
+}
