2a77c1bc8c38f1f826121b0ba1204e7f1a4ca41a
xpandeyed <lalbiharipandeyg@gmail.com>
2022-01-06 14:32:55 +0530

    Nested and inner Classes added.

---
+++ src/NestedAndInnerClasses.kt
@@ -0,0 +1,28 @@
+fun nestedClassManager(){
+    val outerObj = Outer()
+
+    //Outer class constructor is not called
+    val nestedObj = Outer.Nested()
+
+    //for inner class object outer class object must be created that is why we need to call the constructor
+    val innerObj = Outer().Inner()
+}
+
+class Outer{
+
+    var p1 = 0
+
+    class Nested{
+        //we cannot access outer class members and properties inside the nested class
+        fun test(){
+            println("Inside of nested class")
+        }
+    }
+
+    inner class Inner{
+        //outer class members and properties inside the inner class
+        fun test(){
+            println("Inside of inner class. $p1")
+        }
+    }
+}
