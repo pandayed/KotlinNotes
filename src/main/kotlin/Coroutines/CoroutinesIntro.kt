015534cb26d16a7bcd3afe49c259844261563a16
xpandeyed <lalbiharipandeyg@gmail.com>
2022-12-20 15:22:56 +0530

    Basics of coroutines done.

--- src/main/kotlin/Coroutines/CoroutinesIntro.kt
+++ src/main/kotlin/Coroutines/CoroutinesIntro.kt
@@ -1,5 +1,9 @@
 package Coroutines
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
 //Notes from smartherd
@@ -25,5 +29,29 @@ fun coroutineManager(){
     }
     //Threads will run in parallel
 
+    //Background coroutine in some background thread
+    //Main thread is unaware of this coroutine. So main thread won't wait for this.
+    GlobalScope.launch {
+        //Blocks the thread for 500 ms
+        Thread.sleep(500)
+        println(Thread.currentThread().name)
+        //DefaultDispatcher-worker-1 will be printed, if coroutine gets executed
+        delay(500)//Delay suspends the coroutine, and does not affect the thread
+        //delay is a suspend funtion and
+        //any suspend function may cause the coroutine to shift to some another thread. Shifting does not happen always.
+    }
+
+    runBlocking {
+        //This coroutine does not run on any new thread
+        //Rather it runs on the current thread
+        //Creates a coroutine that blocks the current thread
+        delay(100)
+    }
+
+
+    //Blocking main thread so that coroutine can get executed
+    Thread.sleep(2000)
+
+    //Always keep a track of the threads you create
 }
 
